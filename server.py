d88e3041fcb9579ed301d14416d99488e4df61bf
Sung Rung Yoo <slifer4195@gmail.com>
2023-04-08 13:04:25 -0500

    from here

--- server.py
+++ server.py
@@ -102,7 +102,8 @@ def work():
 @app.route("/fix",methods=['POST', 'GET'])
 def work1():
     if request.method == "POST":
-        message = request.get_json()['message']
+        # message = request.get_json()['message']
+        pass
         # if message:
             # return "message received"
     # else:
