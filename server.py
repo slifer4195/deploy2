20fbe54ae7c00d40c9746614f364636dcb0de86e
Sung Rung Yoo <slifer4195@gmail.com>
2023-04-09 00:22:57 -0500

    fe

--- server.py
+++ server.py
@@ -88,19 +88,19 @@ def work():
 @app.route("/fix",methods=['POST', 'GET'])
 def work1():
     print("calling it")
-    if request.method == 'POST':
-    #     pass
-    #     try:
-        message = request.get_json()['message']
-    #         # do something with the message here...
-        msg = message['message']
-        value = msg['value']
-        # analyzeResponseParse = test(value)
-        # formula = analyzeResponseParse[0]
-        # targetCell = analyzeResponseParse[1]
-        # insert(formula, targetCell, wks)
-        print("here", value)
-        return jsonify({'success': True, 'message':value})
+    # if request.method == 'POST':
+    # #     pass
+    # #     try:
+    #     message = request.get_json()['message']
+    # #         # do something with the message here...
+    #     msg = message['message']
+    #     value = msg['value']
+    #     # analyzeResponseParse = test(value)
+    #     # formula = analyzeResponseParse[0]
+    #     # targetCell = analyzeResponseParse[1]
+    #     # insert(formula, targetCell, wks)
+    #     print("here", value)
+    #     return jsonify({'success': True, 'message':value})
     #     except (TypeError, KeyError):
     #         # handle the case where the request payload is invalid or missing the "message" field
     #         return jsonify({'success': False, 'message': 'Invalid or missing request payload'})
