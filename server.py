4d7505d32fb8fa20d1ab56a28d3c1a3c3a4a5179
Sung Rung Yoo <slifer4195@gmail.com>
2023-04-09 00:36:56 -0500

    fwefw

--- server.py
+++ server.py
@@ -27,15 +27,15 @@ login_manager.login_view = 'login'
 CORS(app)
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20), nullable=False, unique=True)
+#     password = db.Column(db.String(80), nullable=False)
 
 
 class RegisterForm(FlaskForm):
